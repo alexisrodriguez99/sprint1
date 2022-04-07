3d91eaaf34afa29011ab3c0afdbbd2d740b0e6aa
Rodriguez Ossa Alexis Kevin <alexisrodriguezossa99@gmail.com>
2022-04-07 11:45:00 -0300

    sprint1 entregar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AhorcadoComponent } from './ahorcado/ahorcado.component';
+import { ErrorComponent } from './error/error.component';
+import { HomeComponent } from './home/home.component';
+import { JuegosComponent } from './juegos/juegos.component';
+import { PreguntadosComponent } from './juegos2022/preguntados/preguntados.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"login", component:LoginComponent},
+  {path:"registro", component:RegistroComponent},
+  {path:"home", component:HomeComponent},
+  {
+    path:'juegos', component:JuegosComponent, children:[
+      {path:'preguntados', component:PreguntadosComponent}
+    ]
+  },
+  /*{path:"juegos", component:JuegosComponent},
+  {path:"juegos/ahorcado", component:AhorcadoComponent},
+  {path:"juegos/preguntados", component:PreguntadosComponent},*/
+  {path:"", redirectTo:"/home", pathMatch:"full"},
+  {path:"**", component:ErrorComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,29 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
  import { QuienSoyComponent } from './quien-soy/quien-soy.component';
+import { FormsModule } from '@angular/forms';
+import { RegistroComponent } from './registro/registro.component';
+import { ErrorComponent } from './error/error.component';
+import { JuegosComponent } from './juegos/juegos.component';
+import { AhorcadoComponent } from './ahorcado/ahorcado.component';
+import { PreguntadosComponent } from './juegos2022/preguntados/preguntados.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-     QuienSoyComponent
+     QuienSoyComponent,
+     RegistroComponent,
+     ErrorComponent,
+     JuegosComponent,
+     AhorcadoComponent,
+     PreguntadosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
